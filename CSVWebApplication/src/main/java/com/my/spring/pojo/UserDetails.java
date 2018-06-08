aef468af829a76c581369e4a4f81131c5423f733
janhavi <janhavi.u@husky.neu.edu>
2018-06-07 22:52:59 -0400

    Initial commit for setting up the basic structure of web app

---
+++ CSVWebApplication/src/main/java/com/my/spring/pojo/UserDetails.java
@@ -0,0 +1,91 @@
+package com.my.spring.pojo;
+
+
+public class UserDetails {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String company;
+	private String email;
+	private String address;
+	private String secondaryAddress;
+	private int zip;
+	private String city;
+	private String stateLong;
+	private String state;
+	private String phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSecondaryAddress() {
+		return secondaryAddress;
+	}
+	public void setSecondaryAddress(String secondaryAddress) {
+		this.secondaryAddress = secondaryAddress;
+	}
+	public int getZip() {
+		return zip;
+	}
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStateLong() {
+		return stateLong;
+	}
+	public void setStateLong(String stateLong) {
+		this.stateLong = stateLong;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+
+}
