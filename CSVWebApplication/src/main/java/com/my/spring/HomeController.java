d30d68fbfa3fc27d34878c86518e43065cbbe68e
janhavi <janhavi.u@husky.neu.edu>
2018-06-08 18:12:35 -0400

    Final Changes done to display duplicates and non duplicate records separately in the required format on jsp.

--- CSVWebApplication/src/main/java/com/my/spring/HomeController.java
+++ CSVWebApplication/src/main/java/com/my/spring/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.my.spring.dao.CSVDao;
 import com.my.spring.pojo.UserDetails;
@@ -23,38 +24,36 @@ import org.apache.commons.lang3.StringUtils;
  */
 @Controller
 public class HomeController {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-
-		String formattedDate = dateFormat.format(date);
-
-		model.addAttribute("serverTime", formattedDate);
-		
+	public ModelAndView home(Locale locale, Model model) {
+	
+		// Parsing the csv file to get content
 		CSVDao csvdao = new CSVDao();
 		ArrayList<UserDetails> list = csvdao.getContent("normal");
 		ArrayList<String> listOfStrings = new ArrayList<String>();
-		HashMap<Integer,ArrayList<String> > map = new HashMap();
-		ArrayList<String> nonDuplicates = new ArrayList<String>();
-		for(UserDetails u : list){
+		HashMap<Integer, ArrayList<String>> map = new HashMap();
+		ArrayList<String> nonDuplicateRecords = new ArrayList<String>();
+		ArrayList<String> duplicateRecords = new ArrayList<String>();
+		// Converting the user details to string with overridden method toString
+		for (UserDetails u : list) {
 			listOfStrings.add(u.toString());
 		}
-		
+
 		for (int i = 0; i < listOfStrings.size(); i++) {
 			ArrayList<String> duplicates = new ArrayList<String>();
 			int count = 0;
-			for (int j = i+1; j < listOfStrings.size(); j++) {
+			for (int j = i + 1; j < listOfStrings.size(); j++) {
+				// Using LevenshteinDistance to find the similarity between the
+				// strings
 				int dist = StringUtils.getLevenshteinDistance(listOfStrings.get(i), listOfStrings.get(j));
-
+				// If the distance is less than 7 means strings can be
+				// considered similar
 				if (dist < 7) {
 					if (count == 0) {
 						duplicates.add(listOfStrings.get(i));
@@ -67,33 +66,38 @@ public class HomeController {
 			}
 			map.put(i, duplicates);
 		}
-		
-		for(Entry<Integer, ArrayList<String>> e : map.entrySet()){
-			
-			if(e.getValue().size() > 1){
-				System.out.println(e.getKey()+ "-->");
-				for(String s : e.getValue()){
-					System.out.println(s);
-				}
-				System.out.println("--------");
-			}
-			
-		}
-		
-		
-		nonDuplicates.addAll(listOfStrings);
-		
-			for(Entry<Integer, ArrayList<String>> e : map.entrySet()){
+		// Extracting all the duplicates from map 
+		for (Entry<Integer, ArrayList<String>> e : map.entrySet()) {
+
+			if (e.getValue().size() > 1) {
 				
-				nonDuplicates.removeAll(e.getValue());
+				duplicateRecords.addAll(e.getValue());
 			}
-			System.out.println("Non-Duplicates");
-			for(String s : nonDuplicates){
-				System.out.println(s);
-			}
-			
-	
-		return "home";
+
+		}
+
+		//Extracting Non duplicates from original list of strings
+		nonDuplicateRecords.addAll(listOfStrings);
+
+		for (Entry<Integer, ArrayList<String>> e : map.entrySet()) {
+
+			nonDuplicateRecords.removeAll(e.getValue());
+		}
+		//Display all duplicates
+		System.out.println("Duplicates");
+		for (String s : duplicateRecords) {
+			System.out.println(s);
+		}
+		//Display all non duplicates
+		System.out.println("Non-Duplicates");
+		for (String s : nonDuplicateRecords) {
+			System.out.println(s);
+		}
+		//creating map to send to view
+		HashMap<String, Object> viewMap = new HashMap();
+		viewMap.put("Duplicates", map);
+		viewMap.put("NonDuplicates", nonDuplicateRecords);
+		return new ModelAndView("home", "viewMap", viewMap);
 	}
-	
+
 }
--- CSVWebApplication/src/main/webapp/WEB-INF/views/home.jsp
+++ CSVWebApplication/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,30 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<h1>CSV Web Application!</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
+	<h2>Duplicate Records</h2>
+	<c:set var="count" value="1" />
+
+	<c:forEach var="viewMap" items="${requestScope.viewMap.Duplicates}">
+		
+		<c:if test="${viewMap.value.size() > 1}"> 
+		
+		<c:out value="Duplicate Set: ${count}" /><br>
+  		 ${viewMap.value}<br>
+			<c:set var="count" value="${count + 1}" />
+		</c:if>
+	</c:forEach>
+
+	<h2>Non Duplicate Records</h2>
+	<c:forEach var="viewMap" items="${requestScope.viewMap.NonDuplicates}">
+
+   ${viewMap}<br>
+	</c:forEach>
+
 </body>
 </html>
